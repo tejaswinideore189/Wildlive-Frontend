e7b4a424a0cc943cce99c536682377b9e4db0d2a
tejaswinideore189 <tejaswinideore189@gmail.com>
2023-07-11 14:51:30 +0530

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
+      <Script
+        id="bootstrap-cdn"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
